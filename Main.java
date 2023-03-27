648022df28fd26a8496c9360fc590d1b29551808
user <bhagyasubhashini139@gmail.com>
2023-03-26 17:10:30 -1200

    Constructor Project Code

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    Main(){
+        System.out.println("Java");
+
+        String name="Progamming Language";
+    }
+
+    public static void main(String[] args) {
+
+        Main m=new Main();
+
+    }
+}
